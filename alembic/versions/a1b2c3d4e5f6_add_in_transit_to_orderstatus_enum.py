391bd708c32608351bd64ecc4202c08b3a07e614
lustrioptom <lustrioptom@users.noreply.github.com>
2026-07-20 22:14:11 +0300

    Track alembic migrations + add in_transit enum value for PostgreSQL

---
+++ alembic/versions/a1b2c3d4e5f6_add_in_transit_to_orderstatus_enum.py
@@ -0,0 +1,24 @@
+"""add in_transit to orderstatus enum
+
+Revision ID: a1b2c3d4e5f6
+Revises: 5c4ce6d4334d
+Create Date: 2026-07-20 12:00:00.000000
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = 'a1b2c3d4e5f6'
+down_revision: Union[str, None] = '5c4ce6d4334d'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute("ALTER TYPE orderstatus ADD VALUE IF NOT EXISTS 'in_transit'")
+
+
+def downgrade() -> None:
+    pass
